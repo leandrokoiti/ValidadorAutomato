d2334576ff9f69211322de7f1a1f33e2599abdab
LeandroKoiti <LeandroKoiti@LEANDROKOITI-PC>
2016-05-23 23:35:33 -0400

    Renomeado o projeto "ValidarAutomato" para "ValidadorAutomato" para poder substituir corretamente o projeto antigo.

---
+++ ValidadorAutomato/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ValidadorAutomato
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            // Usado para abrir o index.html
+            app.UseDefaultFiles();
+
+            // Usado para serviro os arquivos em wwwroot/
+            app.UseStaticFiles();
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
